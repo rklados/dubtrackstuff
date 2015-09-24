0aeaf1f6162ff0c52383ef48ad271dc4c88e570e
Robert Klados <rklados@gmail.com>
2015-09-23 23:08:05 -0600

    Create autodubplus.js
    user enter/leave for autodub

---
+++ autodubplus.js
@@ -0,0 +1,65 @@
+var autoDub = {
+	started: false,
+	version: "00.02"
+}
+var userList= [];
+
+autoDub.userEnterLeave = function() {
+
+	nowUsers = autoDub.getUsers();
+
+	$.each(nowUsers, function(index, user) {
+		if (nowUsers.length > userList.length)
+		{
+			entertext = " Entered!";		
+		} else {
+			entertext = " Left!";
+		}
+
+		if ($.inArray(user, userList) == -1)
+		{
+			console.log(user + entertext);
+			$("#rob-msgs").html(user + entertext);
+		}
+	});
+}
+
+autoDub.getUsers = function()
+{
+	users = [];
+	$("#avatar-list li p").each(function(one, thing) { 
+
+	if (thing.className == 'username')
+	{
+		users.push($(thing).html());
+	}
+	
+	});
+
+	return users;
+}
+
+autoDub.newSong = function(){
+	var songName = $(".currentSong").text();
+	if (songName == "loading...") return;
+	$(".dubup").click();
+	console.log("voted for "+songName);
+};
+
+autoDub.init = function(){
+	$('.currentSong').bind("DOMSubtreeModified", autoDub.newSong);
+	
+	$(".dubup").click();
+
+		
+	$('.currentSong').bind("DOMSubtreeModified", autoDub.userEnterLeave);
+
+	userList = autoDub.getUsers();
+	$("#pusher-chat-widget-input").append(
+			"<p><div id='rob-msgs' style='color:red;'></div></p>"
+	);
+
+	console.log("autodub v"+autoDub.version+" is a go!");
+};
+
+if (!autoDub.started) autoDub.init();
